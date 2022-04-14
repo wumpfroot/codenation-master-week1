6858ef2719f38ed9e2c96b4c12862b77b19729dc
wumpfroot <markuskojo@msn.com>
2022-04-14 18:18:57 +0100

    Object activities

---
+++ objects.js
@@ -0,0 +1,58 @@
+//Activity 1
+
+let person = {
+    name: "Markus",
+    age: 29,
+    sayHi(){
+        return `Hello my name is ${person.name}`
+    }
+}
+
+person.favSongs = [
+    "Ghost - Hunter's Moon", 
+    "Metric - Monster Hospital", 
+    "Tremonti - Cauterize"]
+
+console.log(person.sayHi());
+
+
+//Activity 2
+
+let pet = {
+    name: "Rufus",
+    typeofPet: "Dog",
+    age: 7,
+    colour: "Dark Brown",
+    eat() {
+        return `${pet.name} is eating`
+    },
+    drink() {
+        return `${pet.name} is drinking`
+    },
+    
+}
+
+
+console.log(pet.drink())
+
+
+//Activity 3
+let price = 0
+let coffeeShop = {
+    branch: "Manchester",
+    drinksWithPrices: "Cappuccino",
+    drinkPrice: 2,
+    foodWithPrices: "Carrot cake",
+    foodPrice: 4,
+    drinksOrdered() {
+        price += this.drinkPrice
+        return `${this.drinksWithPrices} is £${this.drinkPrice}. Total price is totaled £${price} `
+    },
+    foodOrdered() {
+        price += this.foodPrice
+        return `${this.foodWithPrices} is £${this.foodPrice} Total price is totaled £${price} `
+    },
+}
+
+console.log(coffeeShop.drinksOrdered())
+console.log(coffeeShop.foodOrdered())
